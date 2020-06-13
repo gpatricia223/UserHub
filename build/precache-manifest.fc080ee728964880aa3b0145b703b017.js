38bb196d6f4aa803711fec8fec3fbd916acf1b19
Patricia Deal <gpatricia223@gmail.com>
2020-06-13 13:21:05 -0700

    finished UserHub

---
+++ build/precache-manifest.fc080ee728964880aa3b0145b703b017.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be079af634c54fe062b1c6a4c2440b81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "daee11d0a8b5e2ac7fcc",
+    "url": "/static/css/main.d6d08344.chunk.css"
+  },
+  {
+    "revision": "057822a738ef70c344fc",
+    "url": "/static/js/2.3daa1c74.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3daa1c74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daee11d0a8b5e2ac7fcc",
+    "url": "/static/js/main.8b978d4d.chunk.js"
+  },
+  {
+    "revision": "16385764b937c4287c41",
+    "url": "/static/js/runtime-main.0f4090cb.js"
+  }
+]);
